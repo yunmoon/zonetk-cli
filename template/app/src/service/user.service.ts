49db0aed491853ae30ca7f9316b49a700bf37bda
yunmoon <meiyun@zhiyuanhui.com>
2019-08-06 20:06:47 +0800

    update create:service template

--- template/app/src/service/user.service.ts
+++ template/app/src/service/user.service.ts
@@ -2,7 +2,7 @@ import { provide, BaseService } from "zonetk-core"
 import { User } from "../entity/user";
 
 @provide("userService")
-export default class UserService extends BaseService {
+export default class UserService extends BaseService<User> {
 
   constructor() {
     super(User);
